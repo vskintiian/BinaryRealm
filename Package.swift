2c9b30a46108a2df1652783f3bf12bd873ba1ac4
Vladyslav Skintiian <vskintiyan@gmail.com>
2022-03-23 13:27:25 +0200

    Update Package.swift
    Updated to 10.24.2 version

--- Package.swift
+++ Package.swift
@@ -3,12 +3,12 @@
 
 import PackageDescription
 
-let realmVersion = "10.21.1"
+let realmVersion = "10.24.2"
 
 func buildTargets() -> [Target] {
     let baseURL = "https://github.com/vskintiian/BinaryRealm/releases/download/\(realmVersion)"
-    let realmChecksum = "7d04cbeec6ae3e739ab0a68ce97dd29511733612a40b15990acaae5dba6a6b63"
-    let realmSwiftChecksum = "4bb54207b7f62ed5f041b06963c72d59c75ff57fed3100a87123766927659bf7"
+    let realmChecksum = "d207a7ab1f3257ac644a2d13bae6f990f32f874453313fb509dd75d91e7116df"
+    let realmSwiftChecksum = "bb8243db3a14a1df47c0239fea09f5a38eff526cced3c0f71234b650be1c534c"
     
     return [
         .binaryTarget(
